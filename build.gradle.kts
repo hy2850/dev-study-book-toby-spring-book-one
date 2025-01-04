f2c70a3f8bf96bc6e0e6817356c1b5c16ff1a261
hy2850 <hy2850@gmail.com>
2025-01-04 12:14:45 +0900

    [Setup] feat: H2 DB 셋업 (Spring boot, spring-data-jpa 필요)
    spring boot 필요
    https://stackoverflow.com/questions/73548351/how-to-access-the-h2-console-for-spring-boot-without-spring-web
    
    baeldung 튜토리얼
    https://www.baeldung.com/spring-boot-h2-database
    
    data.sql 실행 옵션 관련
    https://docs.spring.io/spring-boot/how-to/data-initialization.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
-//    kotlin("plugin.spring") version "1.9.25"
-//    id("org.springframework.boot") version "3.4.1"
-//    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.hcpark"
@@ -19,11 +19,16 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter")
-//    implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // 없으면 repository 생성이 안됨; reflection 쓰는 듯
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Setup h2 database
+    // spring boot 필요 - https://stackoverflow.com/questions/73548351/how-to-access-the-h2-console-for-spring-boot-without-spring-web
+    runtimeOnly("com.h2database:h2:2.2.220")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // data.sql 실행해서 h2에 테이블 생성하기 위해 필요
 }
 
 kotlin {
