491f621986f739ff8fd3f1910cff60bac1da3303
hy2850 <hy2850@gmail.com>
2025-01-04 11:31:13 +0900

    feat: initial project setup

---
+++ src/main/kotlin/com/hcpark/springbook/Main.kt
@@ -0,0 +1,10 @@
+package com.hcpark.springbook
+
+//@SpringBootApplication
+//class Main
+
+fun main(args: Array<String>) {
+//    runApplication<DevStudyBookTobySpringBookOneApplication>(*args)
+
+    println("hello world")
+}
---
+++ src/test/kotlin/com/hcpark/springbook/DevStudyBookTobySpringBookOneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hcpark.springbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevStudyBookTobySpringBookOneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
