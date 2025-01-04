6db73545f36a97a6c7807e0570c361155fe4d873
hy2850 <hy2850@gmail.com>
2025-01-04 12:38:04 +0900

    [Setup] feat: 웹 h2 console 접속하려면 Tomcat 서버 띄워야 함 (spring-boot-starter-web 필요)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect") // 없으면 repository 생성이 안됨; reflection 쓰는 듯
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Setup h2 database
-    // spring boot 필요 - https://stackoverflow.com/questions/73548351/how-to-access-the-h2-console-for-spring-boot-without-spring-web
+    // h2 띄우려면 spring boot 필요 - https://stackoverflow.com/questions/73548351/how-to-access-the-h2-console-for-spring-boot-without-spring-web
     runtimeOnly("com.h2database:h2:2.2.220")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa") // data.sql 실행해서 h2에 테이블 생성하기 위해 필요
+//    implementation("org.springframework.boot:spring-boot-starter") // spring-boot-starter-web에 포함됨 (Tomcat 내장서버 없음)
+    implementation("org.springframework.boot:spring-boot-starter-web") // 브라우저로 h2 web console 접속하고 싶으면, Tomcat 내장서버 띄워야 접속 가능
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 kotlin {
