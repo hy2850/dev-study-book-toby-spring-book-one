68c1682546b07f176925cc08d04dde64330ebe37
hy2850 <hy2850@gmail.com>
2025-01-04 16:42:32 +0900

    [Ch2] feat: countAll, deleteAll TC 작성 (TDD)

--- src/test/kotlin/com/hcpark/springbook/user/dao/UserDaoTest.kt
+++ src/test/kotlin/com/hcpark/springbook/user/dao/UserDaoTest.kt
@@ -26,4 +26,43 @@ class UserDaoTest {
         assertEquals(user1.name, userFound.name)
         assertEquals(user1.password, userFound.password)
     }
+
+    @Test
+    fun countAll() {
+        val ctx = AnnotationConfigApplicationContext(DaoFactory::class.java)
+        val dao = ctx.getBean("userDao", UserDao::class.java)
+
+        assertEquals(0, dao.countAll())
+
+        val user1 = User("1", "Kim", "123")
+        dao.add(user1)
+
+        val user2 = User("2", "Park", "123")
+        dao.add(user2)
+
+        assertEquals(2, dao.countAll())
+    }
+
+    @Test
+    fun deleteAll() {
+        // given
+        val ctx = AnnotationConfigApplicationContext(DaoFactory::class.java)
+        val dao = ctx.getBean("userDao", UserDao::class.java)
+
+        val user1 = User("1", "Kim", "123")
+        dao.add(user1)
+
+        val user2 = User("2", "Park", "123")
+        dao.add(user2)
+
+        val beforeCount = dao.countAll()
+        assertEquals(2, beforeCount)
+
+        // when
+        dao.deleteAll()
+
+        // then
+        val afterCount = dao.countAll()
+        assertEquals(0, afterCount)
+    }
 }
