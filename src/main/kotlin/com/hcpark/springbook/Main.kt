000755dbae1adb8fe849dcd64be113971fac0824
hy2850 <hy2850@gmail.com>
2025-01-04 12:43:31 +0900

    [Ch1] JDBC 쓰는 초난감 UserDao

--- src/main/kotlin/com/hcpark/springbook/Main.kt
+++ src/main/kotlin/com/hcpark/springbook/Main.kt
@@ -1,10 +1,24 @@
 package com.hcpark.springbook
 
-//@SpringBootApplication
-//class Main
+import com.hcpark.springbook.user.dao.UserDao
+import com.hcpark.springbook.user.domain.User
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Main
 
 fun main(args: Array<String>) {
-//    runApplication<DevStudyBookTobySpringBookOneApplication>(*args)
+    runApplication<Main>(*args)
+
+    val dao = UserDao()
+
+    val user1 = User("1", "Kim", "123")
+    dao.add(user1)
+    println("user added, id: ${user1.id}")
+
+    val userFound = dao.get(user1.id)
+    println(userFound)
 
     println("hello world")
 }
