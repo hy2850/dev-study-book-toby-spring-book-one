0fd92e43c530ba8aee16b6fcc1ac6bf0bab2fc82
hy2850 <hy2850@gmail.com>
2025-01-04 18:27:38 +0900

    [Ch2] refactor: JunitTest TC 리팩토링 - Kotlin MutableSet 활용

--- src/test/kotlin/com/hcpark/springbook/JUnitTest.kt
+++ src/test/kotlin/com/hcpark/springbook/JUnitTest.kt
@@ -1,43 +1,38 @@
 package com.hcpark.springbook
 
-import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
+// 매 @Test마다 새로운 테스트 클래스 인스턴스가 생성되는지 TC
 class JUnitTest {
 
-    // 매 @Test마다 새로운 테스트 클래스 인스턴스가 생성되는지 TC
-
     companion object {
-        private var previousInstanceHashCode: Int? = null
+        private var testObjects: MutableSet<Int> = HashSet()
     }
 
     @Test
     fun testInstanceCreation1() {
         val currentInstanceHashCode = this.hashCode()
-        if (previousInstanceHashCode != null) {
-            println("previousInstanceHashCode: $previousInstanceHashCode, currentInstanceHashCode: $currentInstanceHashCode")
-            assertNotEquals(previousInstanceHashCode, currentInstanceHashCode, "Test class instance should be different")
+        if (testObjects.contains(currentInstanceHashCode)) {
+            throw AssertionError("Test class instance should be different")
         }
-        previousInstanceHashCode = currentInstanceHashCode
+        testObjects.add(currentInstanceHashCode)
     }
 
     @Test
     fun testInstanceCreation2() {
         val currentInstanceHashCode = this.hashCode()
-        if (previousInstanceHashCode != null) {
-            println("previousInstanceHashCode: $previousInstanceHashCode, currentInstanceHashCode: $currentInstanceHashCode")
-            assertNotEquals(previousInstanceHashCode, currentInstanceHashCode, "Test class instance should be different")
+        if (testObjects.contains(currentInstanceHashCode)) {
+            throw AssertionError("Test class instance should be different")
         }
-        previousInstanceHashCode = currentInstanceHashCode
+        testObjects.add(currentInstanceHashCode)
     }
 
     @Test
     fun testInstanceCreation3() {
         val currentInstanceHashCode = this.hashCode()
-        if (previousInstanceHashCode != null) {
-            println("previousInstanceHashCode: $previousInstanceHashCode, currentInstanceHashCode: $currentInstanceHashCode")
-            assertNotEquals(previousInstanceHashCode, currentInstanceHashCode, "Test class instance should be different")
+        if (testObjects.contains(currentInstanceHashCode)) {
+            throw AssertionError("Test class instance should be different")
         }
-        previousInstanceHashCode = currentInstanceHashCode
+        testObjects.add(currentInstanceHashCode)
     }
 }
