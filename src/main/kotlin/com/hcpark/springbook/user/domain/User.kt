ca8c6b6aa3aa2894df38f5e24b468e6e10cdd696
hy2850 <hy2850@gmail.com>
2025-01-05 20:15:57 +0900

    [Ch5] feat: 비즈니스 로직 추가 - User에 필드 추가

--- src/main/kotlin/com/hcpark/springbook/user/domain/User.kt
+++ src/main/kotlin/com/hcpark/springbook/user/domain/User.kt
@@ -1,3 +1,10 @@
 package com.hcpark.springbook.user.domain
 
-data class User(val id: String = "", val name: String = "", val password: String = "")
+data class User(
+    val id: String = "",
+    val name: String = "",
+    val password: String = "",
+    val level: Level = Level.BASIC,
+    val loginCnt: Int = 0,
+    val recommendCnt: Int = 0,
+)
