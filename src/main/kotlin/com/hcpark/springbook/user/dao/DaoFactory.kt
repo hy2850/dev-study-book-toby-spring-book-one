1d83085567115fefb3d7adef95aaa7cae94d508b
hyuncheol-park <hyuncheol.park@navercorp.com>
2025-01-05 12:34:08 +0900

    [Ch3] feat: jdbcContext 빈 제거, 대신 userDao 내부에서 생성

--- src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
+++ src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
@@ -8,15 +8,18 @@ class DaoFactory {
 
     @Bean
     fun userDao(): UserDao {
-        return UserDao(jdbcContext())
+//        return UserDao(jdbcContext())
+        return UserDao().apply {
+            setConectionMaker(connectionMaker())
+        }
     }
 
-    @Bean
-    fun jdbcContext(): JdbcContext {
-        val jdbcContext = JdbcContext()
-        jdbcContext.setConnectionMaker(connectionMaker())
-        return jdbcContext
-    }
+//    @Bean
+//    fun jdbcContext(): JdbcContext {
+//        val jdbcContext = JdbcContext()
+//        jdbcContext.setConnectionMaker(connectionMaker())
+//        return jdbcContext
+//    }
 
     @Bean
     fun connectionMaker(): ConnectionMaker {
