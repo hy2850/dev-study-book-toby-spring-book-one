2981a96ee74c7fcd91305814a8276b904135c496
hyuncheol-park <hyuncheol.park@navercorp.com>
2025-01-05 12:22:24 +0900

    [Ch3] feat: 전략패턴 - ResultSet handler 전략도 받도록 (functional interface/lambda 극한 활용)

--- src/main/kotlin/com/hcpark/springbook/user/dao/JdbcContext.kt
+++ src/main/kotlin/com/hcpark/springbook/user/dao/JdbcContext.kt
@@ -3,6 +3,7 @@ package com.hcpark.springbook.user.dao
 import com.hcpark.springbook.user.strategy.StatementStrategy
 import java.sql.Connection
 import java.sql.PreparedStatement
+import java.sql.ResultSet
 import java.sql.SQLException
 
 class JdbcContext {
@@ -14,6 +15,17 @@ class JdbcContext {
     }
 
     fun workWithStatementStrategy(strategy: StatementStrategy) {
+        commonExceptionHandlerTemplate<Boolean>(strategy)
+    }
+
+    fun <T> workWithStatementStrategyAndResultSet(strategy: StatementStrategy, resultSetHandler: (ResultSet) -> T): T {
+        return commonExceptionHandlerTemplate(strategy, resultSetHandler)
+    }
+
+    private fun <T> commonExceptionHandlerTemplate(
+        strategy: StatementStrategy,
+        resultSetHandler: ((ResultSet) -> T)? = null
+    ): T {
         var c: Connection? = null
         var ps: PreparedStatement? = null
 
@@ -22,9 +34,27 @@ class JdbcContext {
 
             ps = strategy.makePreparedStatement(c)
 
-            ps.execute()
+//            // 1. just execute, return nothing
+//            ps.execute()
+//
+//            // 2. execute and handle ResultSet
+//            val rs = ps.executeQuery()
+//            rs.use {
+//                // return result
+//            }
+
+            return if (resultSetHandler != null) {
+                ps.executeQuery().use { rs ->
+                    resultSetHandler(rs)
+                }
+            }
+            else {
+                ps.execute() as T
+            }
+
+//            return resultSetHandler?.let { return ps.executeQuery().use(it) } ?: ps.execute() as T
         } catch (e: SQLException){
-            throw e
+            throw RuntimeException("Error while accessing the database", e)
         }
         finally {
             try {
