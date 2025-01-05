acbe76a5eb11d68e8d8773ae36f79253b4b0afc8
hy2850 <hy2850@gmail.com>
2025-01-05 18:08:24 +0900

    issue: H2 접속 정보로 DataSource 생성 시, data.sql을 이용한 초기화가 안됨

--- src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
+++ src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
@@ -21,6 +21,17 @@ class DaoFactory {
 //        return jdbcContext
 //    }
 
+    // DataSource 생성하면 data.sql로 초기화하는 작업이 안돼서 USERS 테이블이 없음
+//    @Bean
+//    fun dataSource(): DataSource {
+//        return SimpleDriverDataSource().apply {
+//            setDriverClass(org.h2.Driver::class.java)
+//            url = "jdbc:h2:mem:testdb;INIT=RUNSCRIPT FROM 'classpath:data.sql'"
+//            username = "sa"
+//            password = ""
+//        }
+//    }
+
     @Bean
     fun connectionMaker(): ConnectionMaker {
         return H2ConnectionMaker()
