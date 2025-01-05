77c75711915f093c5042c4e90ce876433b79d016
hy2850 <hy2850@gmail.com>
2025-01-05 20:30:19 +0900

    [Ch5] feat: 비즈니스 로직 추가 - jdbc 및 data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-create table if not exists users(id varchar(255) primary key, name varchar(255), password varchar(255))
+create table if not exists users(id varchar(255) primary key, name varchar(255), password varchar(255), level int, loginCnt int, recommendCnt int);
