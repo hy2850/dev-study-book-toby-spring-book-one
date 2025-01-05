bbfbf13a3b9d5d49498a2e78ee400bbdb04c6ec4
hy2850 <hy2850@gmail.com>
2025-01-05 19:00:55 +0900

    fix: 이미 생성된 DataSource 주입받아서 사용하기

--- src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
+++ src/main/kotlin/com/hcpark/springbook/user/dao/DaoFactory.kt
@@ -2,15 +2,15 @@ package com.hcpark.springbook.user.dao
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
 @Configuration
 class DaoFactory {
 
     @Bean
-    fun userDao(): UserDao {
-//        return UserDao(jdbcContext())
+    fun userDao(dataSource: DataSource): UserDao {
         return UserDao().apply {
-            setConectionMaker(connectionMaker())
+            setDataSource(dataSource)
         }
     }
 
@@ -21,19 +21,30 @@ class DaoFactory {
 //        return jdbcContext
 //    }
 
-    // DataSource 생성하면 data.sql로 초기화하는 작업이 안돼서 USERS 테이블이 없음
+    // DataSource 다시 생성하지 말기, 이미 application.yaml 설정 파일 기반으로 하나 생성된 상태라서 충돌 일어남
 //    @Bean
 //    fun dataSource(): DataSource {
-//        return SimpleDriverDataSource().apply {
+//        val dataSource = SimpleDriverDataSource().apply {
 //            setDriverClass(org.h2.Driver::class.java)
-//            url = "jdbc:h2:mem:testdb;INIT=RUNSCRIPT FROM 'classpath:data.sql'"
+//            url = "jdbc:h2:mem:testdb"
 //            username = "sa"
 //            password = ""
 //        }
+//
+//        dataSource.connection.use {
+//            it.createStatement().use { stmt ->
+//                stmt.executeUpdate("CREATE TABLE USERS (ID VARCHAR(10) PRIMARY KEY, NAME VARCHAR(20), PASSWORD VARCHAR(10))")
+//            }
+//        }
+//
+//        return dataSource
 //    }
 
-    @Bean
-    fun connectionMaker(): ConnectionMaker {
-        return H2ConnectionMaker()
-    }
+//    @Bean
+//    fun connectionMaker(): ConnectionMaker {
+//        var template = JdbcTemplate(SimpleDriverDataSource())
+//        template.update("hello")
+
+//        return H2ConnectionMaker()
+//    }
 }
