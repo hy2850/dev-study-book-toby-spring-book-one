c8701d79f6fd2728582aad0ffc5e2b0b1975a512
hy2850 <hy2850@gmail.com>
2025-01-19 09:24:00 +0900

    [Ch5] feat: 비즈니스 로직 추가 - upgradeLevels TDD

--- src/main/kotlin/com/hcpark/springbook/user/service/UserService.kt
+++ src/main/kotlin/com/hcpark/springbook/user/service/UserService.kt
@@ -1,7 +1,31 @@
 package com.hcpark.springbook.user.service
 
 import com.hcpark.springbook.user.dao.UserDao
+import com.hcpark.springbook.user.domain.Level
+import com.hcpark.springbook.user.domain.User
 
 class UserService(private val userDao: UserDao) {
+
+    fun upgradeLevels(users: List<User>) {
+        users.forEach {
+            var isChanged = false
+            if (it.level == Level.BASIC && it.loginCnt >= MIN_LOGCOUNT_FOR_SILVER) {
+                it.level = Level.SILVER
+                isChanged = true
+            } else if (it.level == Level.SILVER && it.recommendCnt >= MIN_RECOMMEND_FOR_GOLD) {
+                it.level = Level.GOLD
+                isChanged = true
+            }
+
+            if (isChanged) {
+                userDao.update(it)
+            }
+        }
+    }
+
+    companion object {
+        const val MIN_LOGCOUNT_FOR_SILVER = 50
+        const val MIN_RECOMMEND_FOR_GOLD = 30
+    }
 
 }
--- src/test/kotlin/com/hcpark/springbook/user/service/UserServiceTest.kt
+++ src/test/kotlin/com/hcpark/springbook/user/service/UserServiceTest.kt
@@ -1,7 +1,11 @@
 package com.hcpark.springbook.user.service
 
 import com.hcpark.springbook.user.dao.UserDao
+import com.hcpark.springbook.user.domain.Level
+import com.hcpark.springbook.user.domain.User
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.Test
@@ -12,8 +16,56 @@ class UserServiceTest {
     @Autowired
     private lateinit var userService: UserService
 
+    @Autowired
+    private lateinit var dao: UserDao
+
+    companion object {
+        private val userPark = User("park", "박씨", "123", Level.BASIC, 49, 0)
+        private val userKim = User("kim", "김씨", "567", Level.BASIC, 50, 0)
+        private val userLee = User("lee", "이씨", "8910", Level.SILVER, 65, 29)
+        private val userGo = User("go", "고씨", "111213", Level.SILVER, 65, 30)
+        private val userKwon = User("kwon", "권씨", "141516", Level.GOLD, 65, 30)
+
+//            User("park", "박씨", "123", Level.BASIC, 49, 0),
+//            User("kim", "김씨", "567", Level.BASIC, 50, 0),
+//            User("lee", "이씨", "8910", Level.SILVER, 65, 29),
+//            User("go", "고씨", "111213", Level.SILVER, 65, 30),
+//            User("kwon", "권씨", "141516", Level.GOLD, 65, 30),
+
+        private val users: MutableList<User> = mutableListOf(
+            userPark,
+            userKim,
+            userLee,
+            userGo,
+            userKwon
+        )
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        dao.deleteAll()
+
+        users.forEach(dao::add)
+    }
+
     @Test
     fun isBeanCreated() {
         assertNotNull(userService)
+    }
+
+    @Test
+    fun upgradeLevels() {
+        userService.upgradeLevels(users)
+
+        checkLevel(userPark, Level.BASIC)
+        checkLevel(userKim, Level.SILVER)
+        checkLevel(userLee, Level.SILVER)
+        checkLevel(userGo, Level.GOLD)
+        checkLevel(userKwon, Level.GOLD)
+    }
+
+    fun checkLevel(user: User, expectedLevel: Level) {
+        val updatedUser = dao.get(user.id)
+        assertEquals(expectedLevel, updatedUser.level)
     }
 }
