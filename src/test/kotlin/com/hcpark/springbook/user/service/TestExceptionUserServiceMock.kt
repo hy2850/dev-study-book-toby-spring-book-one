f8cce70ed50dd2eb8f49d438ee30fb107baeee55
hyuncheol-park <hyuncheol.park@navercorp.com>
2025-01-26 16:17:39 +0900

    [Ch5] refactor: TestExceptionUserServiceMock, exception id setter 추가

--- src/test/kotlin/com/hcpark/springbook/user/service/TestExceptionUserServiceMock.kt
+++ src/test/kotlin/com/hcpark/springbook/user/service/TestExceptionUserServiceMock.kt
@@ -4,13 +4,18 @@ import com.hcpark.springbook.user.dao.UserDao
 import com.hcpark.springbook.user.domain.User
 import org.springframework.transaction.PlatformTransactionManager
 
-class TestUserService(
-    private val exceptionUserId: String,
+class TestExceptionUserServiceMock(
     transactionManager: PlatformTransactionManager,
     userDao: UserDao,
     userLevelUpgradePolicy: UserLevelUpgradePolicy,
     userMailService: UserMailService
 ) : UserService(transactionManager, userDao, userLevelUpgradePolicy, userMailService) {
+
+    private var exceptionUserId: String = ""
+
+    fun setExceptionUserId(exceptionUserId: String) {
+        this.exceptionUserId = exceptionUserId
+    }
 
     override fun upgradeLevel(user: User) {
         if (user.id == exceptionUserId) {
