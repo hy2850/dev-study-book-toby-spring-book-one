365b9ec30c70a7ed34e99db90cb3c1a7b7ad029b
hyuncheol-park <hyuncheol.park@navercorp.com>
2025-01-26 16:59:42 +0900

    [Ch5] refactor: Use mockito mock to replace TestExceptionUserServiceMock

--- src/test/kotlin/com/hcpark/springbook/user/service/DummyMailSender.kt
+++ src/test/kotlin/com/hcpark/springbook/user/service/DummyMailSender.kt
@@ -3,6 +3,7 @@ package com.hcpark.springbook.user.service
 import org.springframework.mail.MailSender
 import org.springframework.mail.SimpleMailMessage
 
+// Fake mail sending funciton
 class DummyMailSender : MailSender {
 
     override fun send(vararg simpleMessages: SimpleMailMessage?) {}
